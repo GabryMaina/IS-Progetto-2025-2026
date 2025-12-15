df39fb092e1a49924e0b4e6f700d4eb0ec8deca9
Gabriele Mainardi <gabry.mainardi@icloud.com>
2025-12-15 10:23:47 +0100

    V1 fix: corretto package Main

---
+++ src/main/java/it/unibs/is/Main.java
@@ -0,0 +1,8 @@
+package it.unibs.is;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("IS-Progetto 2025-2026 - Versione 1");
+        System.out.println("Avvio applicazione (interazione testuale).");
+    }
+}
